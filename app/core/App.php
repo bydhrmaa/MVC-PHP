480da0e88ed12e230c51cb6a408216bf62d10e5c
Bayu Dharma <bayudharmap@gmail.com>
2023-02-11 00:15:26 +0800

    add controller and method

---
+++ app/core/App.php
@@ -0,0 +1,48 @@
+<?php
+
+class App {
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
+
+
+  public function __construct() {
+    $url = $this->parseURL();
+    
+    //controller
+    if(file_exists('../app/controller/'.ucfirst($url[0]). '.php')) 
+    {
+      $this->controller = $url[0];
+      unset($url[0]);
+    } else {
+      require_once "../app/controller/_404.php";
+    }
+    require_once '../app/controller/' . $this->controller . '.php';
+
+    //method
+    if(isset($url[1]))
+    {
+      if(method_exists($controller, $url[1]))
+      {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    //params
+
+    
+
+  }
+
+  public function parseURL() {
+    if(isset($_GET['url'])) 
+    {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
+    
+  }
+}
