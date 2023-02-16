b14b788693e11aff6d5294f53f47687346daf111
Bayu Dharma <bayudharmap@gmail.com>
2023-02-16 22:48:01 +0800

    add nav

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -9,5 +9,15 @@
   <link rel="stylesheet" href="<?=BASE_URL;?>/css/styles.css">
 </head>
 <body>
-  
+  <nav>
+        <div class="navbar navbar-expand-lg border mb-4 bg-navbar">
+          <div class="container-fluid ">
+            <a href="<?=BASE_URL;?>/" class="navbar-brand fw-semibold ms-5 py-3">mengAdu</a>
+          </div>
+        </div>
+  </nav>
+
+    
+   
+
 
